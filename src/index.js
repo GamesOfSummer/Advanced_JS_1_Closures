ca6bc6115370bc089b30a48e6c44fa17e741fc78
Summer T <summer@summertime-development.com>
2020-08-07 13:57:13 -0500

    exploring examples

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 //A closure is a function having access to the parent scope, even after the parent function has closed.
-
 //In JavaScript, closures are created every time a function is created, at function creation time.
-
 function add() {
-  var counter = 0;
-  return function increaseCounter() {
-    counter += 1;
-    console.log("increaseCounter -> counter", counter);
-    return counter;
-  };
+    var counter = 0;
+    return function increaseCounter() {
+        counter += 1;
+        console.log("increaseCounter -> counter", counter);
+        return counter;
+    };
 }
 add();
 add();
 add();
-
 console.log("increaseCounter -> counter");
-
 console.log("\n==============\n\n");
-
 function createFunction() {
-  return function multBy2(num: number) {
-    return num * 2;
-  };
+    return function multBy2(num) {
+        return num * 2;
+    };
 }
-
-let generatedFunction = createFunction();
+var generatedFunction = createFunction();
 console.log(generatedFunction(3));
-
 console.log("\n==============\n\n");
-
 function createFunction2() {
-  return function multBy2(num: number, num2: number) {
-    return num * num2;
-  };
+    return function multBy2(num, num2) {
+        return num * num2;
+    };
 }
-
-let generatedFunction2 = createFunction2();
+var generatedFunction2 = createFunction2();
 console.log(generatedFunction2(3, 3));
